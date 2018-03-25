74a47ec5a50711f72990601089ef5c238e35a401
ripefruit08 <syk4367@rit.edu>
2018-03-25 17:03:00 -0400

    #1 added swipe to delete feature

--- Timer/SecondViewController.swift
+++ Timer/SecondViewController.swift
@@ -14,7 +14,7 @@ class SecondViewController: UIViewController, UITableViewDataSource, UITableView
     
     @IBOutlet var tableview: UITableView!
     let myarray = ["item1", "item2", "item3"]
-    var moneyarr = [Double]()
+    var timeEntries = [NSManagedObject]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,13 +26,14 @@ class SecondViewController: UIViewController, UITableViewDataSource, UITableView
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        moneyarr.removeAll()
+        // TODO: investigate if this code should be moved into viewDidLoad
+        timeEntries.removeAll()
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let request = NSFetchRequest<NSFetchRequestResult>(entityName: "TimeEntry")
         let fetchedData = appDelegate.getData(request: request)
         for data in fetchedData as! [NSManagedObject]{
-            print(data.value(forKey: "value") as! Double)
-            moneyarr.append(data.value(forKey: "value") as! Double)
+            timeEntries.append(data)
+            //print(data.value(forKey: "value") as! Double)
         }
         tableview.reloadData()
         print("viewDidAppear")
@@ -49,12 +50,12 @@ class SecondViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return moneyarr.count
+        return timeEntries.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "customcell", for: indexPath)
-        cell.textLabel?.text = "\(moneyarr[indexPath.row])"
+        cell.textLabel?.text = "\(timeEntries[indexPath.row].value(forKey: "value") as! Double)"
         return cell
     }
     
@@ -65,8 +66,21 @@ class SecondViewController: UIViewController, UITableViewDataSource, UITableView
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if ( editingStyle == UITableViewCellEditingStyle.delete){
             print("put code for delete action here")
-            let alert = UIAlertController(title: "Delete", message: "This feature still needs to be implemented :(", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            let alert = UIAlertController(title: "Delete", message: "Do you want to delete this entry?", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: {_ in
+                let appDelegate = UIApplication.shared.delegate as! AppDelegate
+                let context = appDelegate.persistentContainer.viewContext
+                let toDelete = self.timeEntries[indexPath.row]
+                do{
+                    context.delete(toDelete);
+                    try context.save()
+                    self.timeEntries.remove(at: indexPath.row)
+                    self.tableview.reloadData()
+                } catch {
+                    print("Failed saving")
+                }
+            }))
             self.present(alert, animated: true, completion: nil)
         }
     }
